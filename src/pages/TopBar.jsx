ef7f3bde02636a22fa8e555cb1583b37ec0e581a
Burchard Tang <burchard.tang121@gmail.com>
2025-05-13 21:32:58 -0400

    Restarting

---
+++ src/pages/TopBar.jsx
@@ -0,0 +1,11 @@
+import { Outlet, Link, useLocation } from "react-router-dom";
+import { useState } from "react";
+
+const Menu = () => {
+    return(
+        <>
+        </>
+    );
+}
+
+export default Menu;
